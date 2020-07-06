e4f980a8259caaa4468af1f6c4cc6e626bb1c8fe
midCenturyDude7 <mgriffes@gmail.com>
2020-07-06 15:21:32 -0700

    Querying Salesforce records with Python

--- salesforce_venv/salesforce.py
+++ salesforce_venv/salesforce.py
@@ -9,10 +9,9 @@ from pandas import DataFrame, read_csv
 import json
 from pprint import pprint as pp
 
-sf = Salesforce(instance_url='https://resilient-wolf-8defoo-dev-ed.lightning.force.com', session_id='')
+# sf = Salesforce(instance_url='https://resilient-wolf-8defoo-dev-ed.lightning.force.com', session_id='')
 
-
-pp(sf)
+# set_option('display.max_columns', 20)
 
 login = json.load(open('login.json'))
 username = login['username']
@@ -21,4 +20,42 @@ token = login['token']
 
 session_id, instance = SalesforceLogin(username=username, password=password, security_token=token)
 
-print(session_id, instance)
+sf = Salesforce(instance=instance, session_id=session_id)
+
+SOQL = 'SELECT Id, Name, StageName, AccountId FROM Opportunity'
+
+def QueryData(SOQL):
+    qryResult = sf.query(SOQL)
+    print("Total records: " + str(qryResult['totalSize']))
+    data = []
+
+    isDone = qryResult['done']
+
+    if isDone:
+        for rec in qryResult['records']:
+            rec.pop('attributes', None)
+            data.append(rec)
+        return data
+    
+    while not isDone:
+        try:
+            if not qryResult['done']:
+                for rec in qryResult['records']:
+                    rec.pop('attributes', None)
+                    data.append(rec)
+                qryResult = sf.query_more(qryResult['nextRecordsUrl'], True)
+            else:
+                for rec in qryResult['records']:
+                    rec.pop('attributes', None)
+                    data.append(rec)
+                    isDone = True
+                print('Query Complete')
+
+        except NameError:
+            for rec in qryResult['records']:
+                rec.pop('attributes', None)
+                data.append(rec)
+            qryResult = sf.query_more(qryResult['nextRecordsUrl'], True)
+    return data
+
+print(DataFrame(QueryData(SOQL)))
